dac4465c5e0b71bd4ea6a79abd92b06983a076b0
Vlad <vladdrozdov16012005@gmail.com>
2026-03-25 15:28:50 +0300

    Stop tracking firebase folder

--- web/src/firebase/uploadImage.js
+++
@@ -1,9 +0,0 @@
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { storage } from './firebase';
-
-export const uploadImage = async (file) => {
-  const storageRef = ref(storage, `images/${file.name}`);
-  await uploadBytes(storageRef, file);
-  const url = await getDownloadURL(storageRef);
-  return url;
-};
